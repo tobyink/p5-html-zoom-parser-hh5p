5678502a2d53d014fadc39f5ddda73d5133dd334
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-28 13:49:35 +0100

    bump version; changelog

--- lib/HTML/Zoom/Parser/HH5P.pm
+++ lib/HTML/Zoom/Parser/HH5P.pm
@@ -7,7 +7,7 @@ use utf8;
 
 BEGIN {
 	$HTML::Zoom::Parser::HH5P::AUTHORITY = 'cpan:TOBYINK';
-	$HTML::Zoom::Parser::HH5P::VERSION   = '0.001';
+	$HTML::Zoom::Parser::HH5P::VERSION   = '0.002';
 }
 
 use HTML::HTML5::Parser;
@@ -305,7 +305,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2012 by Toby Inkster.
+This software is copyright (c) 2012-2013 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
